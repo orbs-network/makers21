b8f77168ddaa0d7522684e15b2982bf16896937f
doronaviguy <doron@orbs.com>
2021-08-04 21:34:50 +0300

    listen to 0000

--- server/static-server.js
+++ server/static-server.js
@@ -4,4 +4,4 @@ const PORT = 1137;
 
 app.use(express.static('../three'));
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server listening on port: ${PORT}`));
