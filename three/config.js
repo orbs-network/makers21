cb0c8ac097f2f813debc5ec8c0e96631dcc7a58a
Yuval Aviguy <yuvala@Yuvals-MBP.lan>
2021-10-11 14:48:22 +0300

    peed size

--- three/config.js
+++ three/config.js
@@ -1,6 +1,8 @@
 
 const SIZE = 200;
 const HEIGHT = 120;
+const GATE_SIZE = SIZE / 20;
+
 const BLUE = 0x224988;
 const RED = 0x703020;
 const BLUE2 = 0x003070;
@@ -15,7 +17,7 @@ const toRad = (Math.PI/180);
 window.config = {
   size:SIZE ,
   distancePerMS : SIZE/4000, // half cort in 4 sec
-  gateTurnPerSec : 0.1,
+  gateTurnPerSec : 0.15,
   //ctrlHeightLimit: (20 * toRad)
   // steerReleaseFactor: 0.95,
   // horizLimit:0.02,
@@ -37,5 +39,5 @@ config.passFar = config.shootFar + 2;
 config.targetLockMs = 1000;
 
 config.colideDistance = config.distancePerMS * 50;
-config.gatePassDistance = config.distancePerMS * 200;
+config.gatePassDistance = config.distancePerMS * 100;
 config.lookSpeed = config.distancePerMS * 0.003;//0.0005,
