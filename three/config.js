f086b36c2364fb94da076fbe8688081f2a5be4d5
Yuval Aviguy <yuvala@Yuvals-MBP.lan>
2021-10-11 13:17:42 +0300

    change size speed raycase

--- three/config.js
+++ three/config.js
@@ -1,6 +1,6 @@
 
-const SIZE = 30;
-const HEIGHT = 15;
+const SIZE = 200;
+const HEIGHT = 120;
 const BLUE = 0x224988;
 const RED = 0x703020;
 const BLUE2 = 0x003070;
@@ -14,7 +14,7 @@ const toRad = (Math.PI/180);
 
 window.config = {
   size:SIZE ,
-  distancePerMS : 3/1000,
+  distancePerMS : SIZE/4000, // half cort in 4 sec
   gateTurnPerSec : 0.1,
   //ctrlHeightLimit: (20 * toRad)
   // steerReleaseFactor: 0.95,
@@ -25,11 +25,11 @@ window.config = {
   explodePartSize: 0.02,
   // rotUnit:0.001,
   raycastNear:0,
-  raycastFar:SIZE*2
+  raycastFar:SIZE
 };
 
 
-config.shootNear = 1;
+config.shootNear = SIZE / 20;
 config.shootFar = config.shootNear + SIZE * 1.3;
 config.passNear = 0;
 config.passFar = config.shootFar + 2;
@@ -37,4 +37,5 @@ config.passFar = config.shootFar + 2;
 config.targetLockMs = 1000;
 
 config.colideDistance = config.distancePerMS * 50;
-config.lookSpeed = config.distancePerMS * 0.1;//0.0005,
+config.gatePassDistance = config.distancePerMS * 200;
+config.lookSpeed = config.distancePerMS * 0.003;//0.0005,
